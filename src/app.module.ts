ab95cfd40f80c8441337a3e7301707e53d36ebb3
oleh19127 <oleh19127@gmail.com>
2024-05-23 14:23:52 +0300

    update app module and main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,53 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { ProfileModule } from './profile/profile.module';
+import { VoteModule } from './vote/vote.module';
+import { LoggerModule } from 'nestjs-pino';
+import { User } from './auth/user.entity';
+import { Profile } from './profile/profile.entity';
+import { Vote } from './vote/vote.entity';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { PasswordToolService } from './utils/password-tool.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
+    }),
+    ConfigModule.forRoot(),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        database: configService.get('POSTGRES_DB'),
+        port: parseInt(configService.get('POSTGRES_PORT'), 10),
+        password: configService.get('POSTGRES_PASSWORD'),
+        username: configService.get('POSTGRES_USER'),
+        entities: [User, Profile, Vote],
+        synchronize: true,
+      }),
+    }),
+    AuthModule,
+    ProfileModule,
+    VoteModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [PasswordToolService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
